6e6695888e9959abc045a4216db976c5898971df
husty530 <husty530@gmail.com>
2021-09-21 21:54:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Listener/Pages/Index.razor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Reactive.Subjects;
+using Microsoft.JSInterop;
+
+namespace Listener.Pages
+{
+    partial class Index
+    {
+
+        private static string _text = "";
+        private static string _positionLeft = "20%";
+        private static string _positionTop = "20%";
+        private static readonly string[] _gainKeys = new string[] { "ちょっと", "かなり" };
+        private static readonly string[] _directionKeys = new string[] { "上に", "下に", "左に", "右に" };
+        private static readonly Subject<bool> _ = new();
+
+        public Index()
+        {
+            _text = "";
+            Task.Run(async () =>
+            {
+                await Task.Run(() => { while (Js is null) ; });
+                await Js.InvokeVoidAsync("startWebSpeech", "Listener");
+            });
+            _.Subscribe(_ => InvokeAsync(() => StateHasChanged()));
+        }
+
+        [JSInvokable]
+        public static void NotifySpeechResult(string text)
+        {
+            _text = text;
+            Interpret(text);
+            _.OnNext(true);
+        }
+
+        private static void Interpret(string text)
+        {
+            var gain = 8;
+            for (int i = 0; i < _gainKeys.Length; i++)
+                if (text.Contains(_gainKeys[i]))
+                    gain -= (int)Math.Pow(-1, i) * 5;
+            var vertical = 0;
+            var horizonal = 0;
+            for (int i = 0; i < _directionKeys.Length; i++)
+                if (text.Contains(_directionKeys[i]))
+                {
+                    if (i < 2)
+                        vertical -= (int)Math.Pow(-1, i);
+                    else
+                        horizonal -= (int)Math.Pow(-1, i);
+                }
+            if (int.TryParse(_positionLeft.Replace("%", ""), out var left) && int.TryParse(_positionTop.Replace("%", ""), out var top))
+            { 
+                _positionLeft = $"{left + gain * horizonal}%";
+                _positionTop = $"{top + gain * vertical}%";
+            }
+        }
+
+    }
+}
---
+++ Listener/wwwroot/js/speech.js
@@ -0,0 +1,53 @@
+﻿///
+/// Web Speech API
+///
+
+let _app = null;
+let _autoRestart = null;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
+const SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent
+const _speechRecognitionList = new SpeechGrammarList();
+const _recognition = new SpeechRecognition();
+
+// your project name, ja or en-US, string array, bool
+function startWebSpeech(appName, lang = 'ja', autoRestart = true, dictionary = null) {
+    _app = appName;
+    _autoRestart = autoRestart;
+    _recognition.lang = lang;
+    if (dictionary != null) {
+        if (lang == 'ja')
+            _speechRecognitionList.addFromString('#JSGF V1.0 JIS ja; grammar words; public <word> = ' + dictionary.join(' | ') + ' ;', 1);
+        else
+            _speechRecognitionList.addFromString('#JSGF V1.0; grammar words; public <word> = ' + dictionary.join(' | ') + ' ;', 1);
+        _recognition.grammars = _speechRecognitionList;
+    }
+    _recognition.continuous = false;
+    _recognition.interimResults = false;
+    _recognition.maxAlternatives = 1;
+    if (_autoRestart)
+        _recognition.start();
+}
+
+function start() {
+    _recognition.start();
+}
+
+function stop() {
+    _recognition.stop();
+}
+
+_recognition.onspeechend = () => {
+    _recognition.stop();
+}
+
+_recognition.onresult = async e => {
+    const msg = e.results[0][0].transcript;
+    await DotNet.invokeMethodAsync(_app, 'NotifySpeechResult', msg);
+    console.log(msg);
+}
+
+_recognition.onend = () => {
+    if (_autoRestart)
+        _recognition.start();
+}
